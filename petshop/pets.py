199972d033b0cb6e88b62fd36c263afb5d602c2e
abhinavgeethan <abhinavgeethan@gmail.com>
2021-07-14 01:17:54 +0400

    Forcing JSON type

--- petshop/pets.py
+++ petshop/pets.py
@@ -85,8 +85,8 @@ def edit(pid):
         description = request.form.get('description')
         sold = request.form.get("sold")
         if not (sold and description):
-            resp=request.json
-            # print(resp['description'])
+            resp=request.get_json(force=True)
+            print(resp)
             if "sold" in resp.keys():
                 sold=resp['sold']
                 if sold=="on" or sold=='1':
